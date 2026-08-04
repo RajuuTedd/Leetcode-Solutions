31e5b2b0c549ee0934dc87e7e5007ef375169b9a
RajuuTedd <112092916+RajuuTedd@users.noreply.github.com>
2026-08-04 23:32:41 +0530

    Create 695-MaxAreaOfIsland

---
+++ 695-MaxAreaOfIsland/695-MaxAreaOfIsland.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/4/2026, 11:32:41 PM
+class Solution {
+public:
+    int dfs(vector<vector<int>>&grid , int i , int j ){
+        if(i < 0 || i>=grid.size()||j<0||j>=grid[0].size() || grid[i][j]==0){
+            return 0;
+        }
+        grid[i][j] = 0;
+        int area = 1;
+
+        area+= dfs(grid,i-1,j);
+        area+= dfs(grid,i+1,j);
+        area+= dfs(grid,i,j-1);
+        area+= dfs(grid,i,j+1);
+
+        return area;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxarea = 0;
+
+        for(int i = 0 ; i < grid.size();i++){
+            for(int j = 0 ; j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    int area = dfs(grid,i,j);
+                    maxarea = max(maxarea,area);
+                }
+            }
+        }
+        return maxarea;
+    }
+};
